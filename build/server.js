bea70b5327baac5ecb5215e7b2605de3e88922af
david2899 <david@qordev.com>
2022-07-03 15:16:38 -0600

    make it better

--- build/server.js
+++ build/server.js
@@ -1,42 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Server = void 0;
-const express_1 = __importDefault(require("express"));
-const morgan_1 = __importDefault(require("morgan"));
-const helmet_1 = __importDefault(require("helmet"));
-const compression_1 = __importDefault(require("compression"));
-const cors_1 = __importDefault(require("cors"));
-// import routes
-const PostRoutes_1 = __importDefault(require("./routes/PostRoutes"));
-// Server Class
-class Server {
-    constructor() {
-        this.app = express_1.default();
-        this.config();
-        this.routes();
-    }
-    config() {
-        // Settings
-        this.app.set('port', process.env.PORT || 4000);
-        // middlewares
-        this.app.use(morgan_1.default('dev'));
-        this.app.use(express_1.default.json());
-        this.app.use(express_1.default.urlencoded({ extended: false }));
-        this.app.use(helmet_1.default());
-        this.app.use(compression_1.default());
-        this.app.use(cors_1.default());
-    }
-    routes() {
-        const router = express_1.default.Router();
-        this.app.use('/api/', PostRoutes_1.default);
-    }
-    start() {
-        this.app.listen(this.app.get('port'), () => {
-            console.log('Server is listening on port', this.app.get('port'));
-        });
-    }
-}
-exports.Server = Server;
+const app_1 = require("./app");
+require("./config/mongoose");
+const server = new app_1.Server();
+server.start();
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Server } from "./app";
-import './config/mongoose'
-
-const server = new Server();
-server.start();
