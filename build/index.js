8e78d9c4518c25ec8e8c60aebf4eec2e626f4d5f
david2899 <david@qordev.com>
2022-07-03 15:42:29 -0600

    make it better

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-require("./config/mongoose");
-const server = new server_1.Server();
-server.start();
--- build/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-require("./config/mongoose");
-const server = new app_1.Server();
-server.start();
